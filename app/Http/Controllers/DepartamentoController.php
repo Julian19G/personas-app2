a4d2e2a3ef3645aee144afcb1e9424f42d8a3421
Julian Guerra <julian19guerra@gmail.com>
2024-04-02 01:10:46 -0500

    Departamento New blade

--- app/Http/Controllers/DepartamentoController.php
+++ app/Http/Controllers/DepartamentoController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Departamento;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DepartamentoController extends Controller
 {
